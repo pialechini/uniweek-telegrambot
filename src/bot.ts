caed390a862a9d8d7e644b2053c74be4e6cd5201
PialeChini <mobinzohourian@gmail.com>
2023-10-15 10:33:52 +0330

    --amend

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import bot from "./create-bot";
 import { webhookCallback } from "grammy";
 import "./features/week-schedule/index";
-import "./commands.ts";
+import "./commands";
 import "./cache";
 
 bot.api.setMyCommands([
--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,8 @@
 import bot from "./create-bot";
 import { singUp } from "./features/auth/auth";
 
+console.log("commands loaded");
+
 bot.command("start", async (ctx) => {
   ctx.reply("به به! خوش آمدید");
 

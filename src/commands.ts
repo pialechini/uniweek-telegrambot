48177173b9645cef4ac1a1cf76ab7604934813ba
PialeChini <mobinzohourian@gmail.com>
2023-10-15 10:36:55 +0330

    --amend

--- src/commands.ts
+++ src/commands.ts
@@ -1,10 +1,11 @@
+import { CommandContext, Context } from "grammy";
 import bot from "./create-bot";
 import { singUp } from "./features/auth/auth";
 
 console.log("commands loaded");
 
-bot.command("start", async (ctx) => {
-  ctx.reply("به به! خوش آمدید");
+bot.command("start", async (ctx: CommandContext<Context>) => {
+  bot.api.sendMessage(ctx.chat.id, ctx.message?.text ?? "some text");
 
   console.log(ctx.message?.text);
 

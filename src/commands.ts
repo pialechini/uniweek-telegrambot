dd50a77974a0d2aabc395da0b9189dedc8157d45
PialeChini <mobinzohourian@gmail.com>
2023-10-15 10:28:41 +0330

    --amend

--- src/commands.ts
+++ src/commands.ts
@@ -2,7 +2,7 @@ import bot from "./create-bot";
 import { singUp } from "./features/auth/auth";
 
 bot.command("start", async (ctx) => {
-  await ctx.reply("به به! خوش آمدید");
+  ctx.reply("به به! خوش آمدید");
 
   if (ctx.from?.id != null) {
     await singUp(ctx.from?.id, ctx.from?.username);
